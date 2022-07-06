ce7a3ee9c94e70d308665214a94125eb480a3995
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-07-06 22:40:45 +0200

    Update flake.nix file.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,53 @@
 {
   description = "Hugo dev";
+
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
+
   outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib; eachSystem allSystems (system:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      hugo-server = pkgs.writeScriptBin "hugo-server" ''
+        ${pkgs.hugo}/bin/hugo server --enableGitInfo --forceSyncStatic -F
+      '';
+    in {
+      # nix shell
       packages = {
-        hugo = pkgs.stdenvNoCC.mkDerivation rec {
+        default = pkgs.buildEnv {
           name = "hugo-dev-${pkgs.hugo.version}";
-          buildInputs = [ pkgs.hugo pkgs.pandoc ];
+
+          paths = [
+            pkgs.hugo
+            pkgs.pandoc
+          ];
         };
       };
-      defaultPackage = packages.hugo;
+
+      # nix develop
+      devShells = {
+        default = pkgs.buildEnv {
+          name = "hugo-dev-${pkgs.hugo.version}";
+
+          paths = [
+            hugo-server
+            pkgs.hugo
+            pkgs.pandoc
+          ];
+        };
+      };
+
+      apps = {
+        default = {
+          type = "app";
+          program = "${hugo-server}/bin/hugo-server";
+        };
+      };
+
     });
 }
