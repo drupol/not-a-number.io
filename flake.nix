8bdc939f64fbda558876a1541c7a5382db8b3b01
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-06-29 22:25:36 +0200

    flake: modernize

--- flake.nix
+++ flake.nix
@@ -1,42 +1,46 @@
 {
-  description = "Hugo website ";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs @ { self, flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = import inputs.systems;
+  outputs =
+    inputs@{ ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-    perSystem = { config, self', inputs', pkgs, system, lib, ... }:
-      let
-        hugo-server = pkgs.writeScriptBin "hugo-server" ''
-          ${lib.getExe pkgs.hugo} server --enableGitInfo --forceSyncStatic -F
-        '';
-      in
-      {
-        # nix develop
-        devShells = {
-          default = pkgs.stdenvNoCC.mkDerivation {
-            name = "hugo-devshell";
-            buildInputs = [
-              hugo-server
-              pkgs.hugo
-              pkgs.pandoc
-              (pkgs.aspellWithDicts (d: [ d.fr d.en d.en-science d.en-computers ]))
-            ];
+      perSystem =
+        { pkgs, lib, ... }:
+        let
+          hugo-server = pkgs.writeScriptBin "hugo-server" ''
+            ${lib.getExe pkgs.hugo} server --enableGitInfo --forceSyncStatic -F
+          '';
+        in
+        {
+          # nix develop
+          devShells = {
+            default = pkgs.mkShellNoCC {
+              packages = [
+                hugo-server
+                pkgs.hugo
+                pkgs.pandoc
+                (pkgs.aspellWithDicts (d: [
+                  d.fr
+                  d.en
+                  d.en-computers
+                ]))
+              ];
+            };
           };
-        };
 
-        # nix run
-        apps = {
-          default = {
-            type = "app";
-            program = lib.getExe hugo-server;
+          # nix run
+          apps = {
+            default = {
+              type = "app";
+              program = lib.getExe hugo-server;
+            };
           };
-        };
 
-      };
-  };
+        };
+    };
 }
